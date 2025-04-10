e044ae94d66fc9b51d95a8513cc2450c72454a61
mahya m <mahya.m8844@gmail.com>
2025-04-10 12:16:10 +0330

    ch_1

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -1,11 +1,25 @@
-public class Exercises {
+
+public class Main {
 
     /*
         complete this function to check if the input number is prime or not
      */
     public boolean isPrime(long n) {
-        // todo
-        return false;
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2){
+            return true;
+        }
+        if (n % 2 == 0){
+            return false;
+        }
+        for (long i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /*
@@ -15,14 +29,25 @@ public class Exercises {
         if the input is not a fibonacci number with description above, return -1
      */
     public long fibonacciIndex(long n) {
-        // todo
+        if (n < 0) {
+            return -1;
+        }
+        long a = 0, b = 1;
+        long temp = 0;
+        for (long i = 0; a <= n; i++) {
+            temp = b;
+            b = a + b;
+            a = temp;
+            if (a == n) {
+                return i;
+            }
+        }
         return -1;
     }
 
     /*
         you should create a triangle with "*" and return a two-dimensional array of characters based on that
         the triangle's area is empty, which means some characters should be " "
-
         example 1, input = 3:
         *
         **
@@ -38,11 +63,30 @@ public class Exercises {
         the output has to be a two-dimensional array of characters, so don't just print the triangle!
      */
     public char[][] generateTriangle(int n) {
-        // todo
-        return null;
+        char[][] triangle = new char[n][n];
+        if (n <= 0) {
+            return new char[0][0];
+        }
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == n - 1 || i == n - 1) {
+                    triangle[i][j] = '*';
+                } else {
+                    triangle[i][j] = ' ';
+                }
+            }
+        }
+        return triangle;
     }
 
+
     public static void main(String[] args) {
-        // you can test your code here, but then it should be checked with test cases
+        Main ex = new Main();
+        System.out.println(ex.isPrime(1));
+        System.out.println(ex.isPrime(41));
+        System.out.println(ex.isPrime(2455));
+        System.out.println(ex.isPrime(2147483647));
+
+
     }
 }
